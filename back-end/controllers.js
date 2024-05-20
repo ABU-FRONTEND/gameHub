82ebd10d7beaccbd60db6abf95c885044b7619cc
Xushmuratov Ibrohim <xushmurodovibrohim3@gmail.com>
2024-05-20 16:00:20 +0500

    init: start project

---
+++ back-end/controllers.js
@@ -0,0 +1,69 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "./models/User.js";
+
+export async function register(req, res) {
+    try {
+        const { username, nickname, password } = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        const doc = new User({
+            username, nickname, passwordHash: hash
+        })
+
+        const user = await doc.save();
+
+        delete user._doc.passwordHash;
+        const token = jwt.sign(
+            { _id: user._id },
+            'secretKey',
+            {
+                expiresIn: '365d',
+            },
+        );
+        res.status(201).json({
+            ...user._doc,
+            token,
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Failed to register',
+        });
+    }
+}
+
+export async function login(req, res) {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+            });
+        }
+        const isValidPass = await bcrypt.compare(password, user._doc.passwordHash);
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Invalid password or username',
+            });
+        }
+        const token = jwt.sign(
+            { _id: user._id },
+            'secretKey',
+            {
+                expiresIn: '365d',
+            },
+        );
+        delete user._doc.passwordHash;
+        res.json({
+            ...user._doc,
+            token,
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Failed to login',
+        });
+    }
+}
---
+++ back-end/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import { register, login } from "./controllers.js";
+import { registerValidators, loginValidators } from "./validators.js";
+import { handleValidationErrors } from "./utils/handleValidationErrors.js";
+
+mongoose.connect('mongodb+srv://admin:ajcuzU0RBWuMK6ke@cluster0.cnvcwtj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+   console.log('sucsess')
+}).catch((e) => {
+   console.log(e);
+})
+
+const app = express();
+app.use(express.json());
+
+app.listen(1111, () => {
+    console.log("Server started on http://localhost:1111");
+})
+
+app.post("/register", registerValidators, handleValidationErrors, register)
+app.post("/login", loginValidators, handleValidationErrors, login)
