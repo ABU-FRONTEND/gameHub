8351882e35e4fdc01fc251f2cef167fc0a70ae25
Shohjahon-n <pazvat56@gmail.com>
2024-05-20 19:10:36 +0500

    added login page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Layout/Layout'
+import LoginPage from '../pages/loginPage/LoginPage'
+// import SignUp from '../pages/signUp/SignUp'
 export default function App() {
     return (
-        <div>App</div>
+        <Routes>
+            <Route path='/' element={<Layout />} >
+                <Route index path='login' element={<LoginPage />} />
+                {/* <Route index path='signup' element={<SignUp />} /> */}
+            </Route>
+        </Routes>
     )
 }
