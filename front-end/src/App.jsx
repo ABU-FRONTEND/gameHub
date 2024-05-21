8c1db0d5df8cbde9bcf732251d78373b88230336
Shohjahon-n <pazvat56@gmail.com>
2024-05-21 15:00:55 +0500

    added navigate

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './Layout/Layout'
 import LoginPage from './pages/loginPage/LoginPage'
 import SignUp from './pages/signUp/SignUp'
@@ -7,6 +7,7 @@ export default function App() {
         <Routes>
 
             <Route path='/' element={<Layout />} ></Route>
+            <Route index element={<Navigate to='auth/login' />} />
             <Route path='/auth'>
                 <Route index path='login' element={<LoginPage />} />
                 <Route index path='signup' element={<SignUp />} />
