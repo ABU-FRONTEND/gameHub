9f3cc7e45506c89f28f20801babb987822091fb2
Abdusamad Latipov <abdusamadlatipov67@gmail.com>
2024-05-21 14:59:12 +0500

    first commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import {  Routes } from "react-router-dom";
 
 
 export default function App() {
   return (
     <> 
-      <Routes>
+      <Routes/>
     </>
   );
 }
