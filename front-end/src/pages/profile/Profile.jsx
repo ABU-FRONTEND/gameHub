63a1e06f9d5132e2bb6ef2eb8520e028b9c78063
Abdusamad Latipov <abdusamadlatipov67@gmail.com>
2024-05-22 21:10:12 +0500

    fied profile page

--- front-end/src/pages/profile/Profile.jsx
+++ front-end/src/pages/profile/Profile.jsx
@@ -21,36 +21,7 @@ export default function Profile() {
             <MonetizationOnIcon sx={{color: 'gold', width: '100px', height: '100px'}}/>
             <Box sx={{color: 'gold', fontSize: '30px', fontWeight: 'bold'}}>1000</Box>
             <Box component={'form'} sx={{width: '100%', paddingTop: '50px'}}>
-                <Box component={'div'} sx={{width: '100%',columnGap: '10px', display: 'flex', alignItems: 'center' }}>
-    <TextField
-      variant="outlined"
-      value={'username'}
-      InputProps={{
-        readOnly: true,
-      }}
-      sx={{
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': {
-            border: 'none',
-          },
-          '&:hover fieldset': {
-            border: 'none',
-          },
-          '&.Mui-focused fieldset': {
-            border: 'none',
-          },
-        },
-        '& .MuiOutlinedInput-input': {
-          outline: 'none',
-        },
-        background: 'red',
-        borderRadius: '20px',
-        width:'85%'
-      }}
-    />
-    <EditIcon sx={{color: 'black'}}/>
-                </Box>
-                <Box component={'div'} sx={{width: '100%', display: 'flex',columnGap: '10px', alignItems: 'center' }}>
+                <Box component={'div'} sx={{width: '100%',columnGap: '10px',position: 'relative', display: 'flex', alignItems: 'center' }}>
     <TextField
       variant="outlined"
       value={'username'}
@@ -73,17 +44,45 @@ export default function Profile() {
         '& .MuiOutlinedInput-input': {
           outline: 'none',
         },
-        background: 'red',
+        background: '#f5f5f5',
+        borderRadius: '20px',
+      }}
+    />
+    <EditIcon sx={{color: 'black', position: 'absolute', right: '25px', top: '15px'}}/>
+                </Box>
+                <Box component={'div'} sx={{width: '100%',position: 'relative', display: 'flex',columnGap: '10px', alignItems: 'center' }}>
+    <TextField
+      variant="outlined"
+      value={'username'}
+      fullWidth
+      InputProps={{
+        readOnly: true,
+      }}
+      sx={{
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            border: 'none',
+          },
+          '&:hover fieldset': {
+            border: 'none',
+          },
+          '&.Mui-focused fieldset': {
+            border: 'none',
+          },
+        },
+        '& .MuiOutlinedInput-input': {
+          outline: 'none',
+        },
+        background: '#f5f5f5',
         padding: '0',
         marginTop: '20px',
         borderRadius: '20px',
-        width:'85%'
 
       }}
     />
-    <EditIcon sx={{color: 'black'}}/>
+    <EditIcon sx={{color: 'black', position: 'absolute', right: '25px', top: '35px'}}/>
                 </Box>
-                <Button variant="contained" sx={{marginTop: '20px', width: '85%',borderRadius: '20px'}} color="success"> Success</Button>
+                <Button variant="contained" sx={{marginTop: '20px', width: '100%',borderRadius: '20px'}} color="success"> Success</Button>
             </Box>
         </Box>
     );
